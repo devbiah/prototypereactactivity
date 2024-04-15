bd97723d48a7eb47d8ec78a66eb23853a75c91d3
Beatriz I. Silva <emmyisilva@gmail.com>
2024-04-15 11:40:13 -0300

    prototypeact

---
+++ Prototype/src/components/Flexbox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Myflex(){
+    return (
+        <article>
+    <section className="Maior">
+        <div>
+        <button>Item 1</button>
+        </div>
+        <div>
+        <button>Item 2</button>
+        </div>
+        <div>
+        <button>Item 3</button>
+        </div>
+    </section>
+    </article>
+    )
+}
---
+++ Prototype/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import Myflex from './components/Flexbox.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Myflex />
+  </React.StrictMode>,
+)
